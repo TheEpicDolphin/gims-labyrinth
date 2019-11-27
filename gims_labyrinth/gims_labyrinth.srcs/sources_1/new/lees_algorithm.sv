8fc09ff58dccdb0b26c117be9752d1f3762d84f2
gdolphin <gdolphin@mit.edu>
2019-11-27 14:51:25 -0500

    writing lees algorithm

--- gims_labyrinth/gims_labyrinth.srcs/sources_1/new/lees_algorithm.sv
+++ gims_labyrinth/gims_labyrinth.srcs/sources_1/new/lees_algorithm.sv
@@ -20,18 +20,19 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module lees_algorithm #(parameter MAX_OUT_DEGREE = 4, parameter MAX_NODES = 1024)
+module lees_algorithm #(parameter MAX_OUT_DEGREE = 4, parameter MAX_NODES = 1024, parameter BRAM_DELAY_CYCLES)
 (
     input clk,
     input rst,
-    input run,
-    input n_start,
+    input start,
+    input [18:0] start_pos,
     
-    //Set high when we can safely read bram output
-    input bram_ready,    
+    input skel_pixel,
+    input [1:0] pixel_,
+    input visited,
         
-    //Used to index into the bram rows
-    output logic [9:0] n,
+    //Used to index into the maze
+    output logic [18:0] pixel_pos,
     
     //read is set high when reading from any of the brams
     output logic read,
@@ -39,35 +40,70 @@ module lees_algorithm #(parameter MAX_OUT_DEGREE = 4, parameter MAX_NODES = 1024
     output logic [1:0] backpointer,
     //These are set high when writing to the appropriate bram
     output logic write_bp,
+    
     output logic done,
     output logic [2:0] state
     );
     
-    parameter IDLE = 3'b000;
-    parameter FETCH_NEIGHBORS = 3'b001;
-    parameter CONSIDER_NEIGHBOR = 3'b010;
-    parameter CHECK_IF_NEIGHBOR_IN_SPT = 3'b011;
-    parameter WAIT_FOR_HEAP_READY = 3'b100;
-    parameter ADD_TO_FRONTIER = 3'b101; //Checks if adjacents are already included in SPT
-    parameter SELECT_MIN_NODE = 3'b110;
-    parameter DONE = 3'b111;
+    parameter IDLE = 2'b00;
+    parameter FETCH_NEIGHBORS = 2'b01;
+    parameter VALIDATE_NEIGHBORS = 2'b10;
+    parameter DONE = 2'b11;
     
     logic [5:0] idx;
     reg [18:0] queue[0:63];
-    reg visited [];
+    
+    reg [18:0] neighbors[0:MAX_OUT_DEGREE - 1];
+    logic [2:0] i;
+    logic [2:0] j;
     
     always_ff @(posedge clk)begin
         if(rst)begin
-            
+            state <= IDLE;
         end
         else begin
             case(state)
                 IDLE: begin
                     if(start)begin
-                        state <= 
+                        state <= FETCH_NEIGHBORS;
+                        pixel_pos <= start_pos;
+                        idx <= 0;
                     end
                 end
+                FETCH_NEIGHBORS: begin
+                    //right
+                    neighbors[0] <= {queue[0][9:0] + 1, queue[0][18:10]};
+                    //up
+                    neighbors[1] <= {queue[0][9:0], queue[0][18:10] - 1};
+                    //left
+                    neighbors[2] <= {queue[0][9:0] - 1, queue[0][18:10]};
+                    //down
+                    neighbors[3] <= {queue[0][9:0], queue[0][18:10] + 1};
+                    
+                    state <= VALIDATE_NEIGHBORS;
+                    queue <= {queue[], }
+                    idx <= idx - 1;
+                    i <= 0;
+                    j <= 0;
+                end
+                VALIDATE_NEIGHBORS: begin
+                    if(j == MAX_OUT_DEGREE)begin
+                        state <= FETCH_NEIGHBORS;
+                    end
+                    if(i == j + BRAM_DELAY_CYCLES)begin
+                        j <= j + 1;
+                        queue[idx] <= neighbors[j];
+                        idx <= idx + 1;
+                    end
+                    if(i < MAX_OUT_DEGREE)begin
+                        read <= 1;
+                        pixel_pos <= neighbors[i];
+                    end
+                    i <= i + 1;
+                end
+                DONE: begin
                 
+                end
             
             endcase
         end
